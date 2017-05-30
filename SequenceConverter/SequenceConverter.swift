140e8240b8f69ef1845acda469e1e0c93c8ea88b
blob8129 <bloba8129@gmail.com>
2017-05-30 12:39:31 +1000

    Added converter

---
+++ SequenceConverter/SequenceConverter.swift
@@ -0,0 +1,41 @@
+//
+//  CustomFormatter.swift
+//  Ralli
+//
+//  Created by Andrey Volobuev on 29/05/2017.
+//  Copyright © 2017 Mikhail Golovko. All rights reserved.
+//
+
+import Foundation
+
+
+struct SequenceConverter {
+    static func commaSpaceBeforeMiddleFormat(_ items: CustomStringConvertible?...) -> String {
+        return items.toStringWithSeparators(before: ", ", middle: ", ", after: "")
+    }
+    
+    static func middleSpaceFormat(_ items: CustomStringConvertible?...) -> String {
+        return items.toStringWithSeparators(before: "", middle: " ", after: "")
+    }
+}
+
+extension Sequence where Iterator.Element == Optional<CustomStringConvertible>  {
+    
+    func toStringWithSeparators(before: CustomStringConvertible,
+                                middle: CustomStringConvertible,
+                                after : CustomStringConvertible) -> String {
+        
+        let filtred = flatMap { $0 }.filter { $0.description.isEmpty == false }
+        let middleSparators = Array<CustomStringConvertible>(repeating: middle, count: filtred.count)
+        let mergedMiddle = zip(filtred, middleSparators)
+            .enumerated()
+            .flatMap { offset, element in
+                offset != filtred.count - 1 ? [element.0, element.1] : [element.0]
+        }
+        
+        let mergedMiddleStr = mergedMiddle.reduce("") { result, element in
+            return "\(result)\(element)"
+        }
+        return mergedMiddleStr.isEmpty == false ? "\(before)\(mergedMiddleStr)\(after)" : ""
+    }
+}
