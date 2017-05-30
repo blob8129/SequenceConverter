0328eb78e3b46483297dff1f23e7002f7135b5ac
blob8129 <bloba8129@gmail.com>
2017-05-30 12:37:40 +1000

    Initial commit

---
+++ SequenceConverter/SequenceConverter.h
@@ -0,0 +1,19 @@
+//
+//  SequenceConverter.h
+//  SequenceConverter
+//
+//  Created by Andrey Volobuev on 30/05/2017.
+//  Copyright © 2017 blob8129. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SequenceConverter.
+FOUNDATION_EXPORT double SequenceConverterVersionNumber;
+
+//! Project version string for SequenceConverter.
+FOUNDATION_EXPORT const unsigned char SequenceConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SequenceConverter/PublicHeader.h>
+
+
